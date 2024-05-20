841d134fa0dbd05d3d16eb148ea1f4fcdda129af
Stepanije <135726514+DemboDev@users.noreply.github.com>
2024-05-20 16:32:21 +0700

    Change a String type to CellType

--- bin/Cell.java
+++ bin/Cell.java
@@ -1,14 +1,16 @@
+import java.awt.Color;
+
 public class Cell {
     // Координаты клетки
     private double x, y;
     // Вектор перемещения клетки
     private double vx, vy;
     // Тип клетки реализован через строку
-    private String type;
+    private CellType type;
     // Переменная задаёт максимальную скорость перемещения (возможны доработки)
     private static final double MAX_SPEED = 5.0;
     // Создание клетки (конструктор)
-    public Cell(double x, double y, String type) {
+    public Cell(double x, double y, CellType type) {
         this.x = x;
         this.y = y;
         this.type = type;
@@ -29,7 +31,7 @@ public class Cell {
     public double getY() { return y; }
     public double getVx() { return vx; }
     public double getVy() { return vy; }
-    public String getType() { return type; }
+    public CellType getType() { return type; }
     // Установка векторов перемещения клетки
     public void setVx(double vx) { this.vx = vx; }
     public void setVy(double vy) { this.vy = vy; }
